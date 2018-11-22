643e1724ecb673c6aeffb917a96c144e1306a7a9
dhruv.gohil <dhruv.gohil@ishisystems.com>
2018-11-22 00:04:45 -0500

    using singleton taskScheduler, read (1) to (6)

--- src/main/java/com/example/distributedlocktest/StartAction.java
+++ src/main/java/com/example/distributedlocktest/StartAction.java
@@ -1,5 +1,7 @@
 package com.example.distributedlocktest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Component;
@@ -8,11 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class StartAction implements Action<StateMachineConfig.States, StateMachineConfig.Events> {
+    static final Logger log = LoggerFactory.getLogger(StartAction.class);
 
     @Override
     @Transactional
     public void execute(StateContext<StateMachineConfig.States, StateMachineConfig.Events> context) {
         try {
+            log.info("calculating something for 30 sec.. ");
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
--- src/main/java/com/example/distributedlocktest/StateMachineSpawner.java
+++ src/main/java/com/example/distributedlocktest/StateMachineSpawner.java
@@ -1,9 +1,11 @@
 package com.example.distributedlocktest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineBuilder;
 
@@ -16,28 +18,46 @@ public class StateMachineSpawner {
     @Autowired
     StartAction startAction;
 
+    //(4) This is correct way to create BEAN, This can be any ()other  @Configuration bean
     @Bean(name = "stateMachineTaskScheduler")
     public ConcurrentTaskScheduler stateMachineTaskScheduler() {
-        ScheduledThreadPoolExecutor threadPool = new ScheduledThreadPoolExecutor(10);
+        // (6) We need to figure out how many threads are actually engaged for our kind of state machines.
+        //     This depends on how much time our ACTION class is blocking..
+        //     Ideally our action should just trigger http calls to NiFi and must return as soon as possible
+        //     So we should be fine having just around 50 to 100 threads.
+        ScheduledThreadPoolExecutor threadPool = new ScheduledThreadPoolExecutor(1
+                , new CustomizableThreadFactory("my-pool-"));
         ConcurrentTaskScheduler taskScheduler = new ConcurrentTaskScheduler(threadPool);
         return taskScheduler;
     }
 
+    //(5) This is how we should inject singleton reference
+    @Autowired
+    @Qualifier("stateMachineTaskScheduler")
+    public ConcurrentTaskScheduler scheduler;
 
     @PostConstruct
     void postConstruct() throws Exception {
         for (int i = 0; i < 10; i++) {
             try {
-                Thread.sleep(3000);
+                Thread.sleep(3);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             StateMachineBuilder.Builder<StateMachineConfig.States, StateMachineConfig.Events> builder = StateMachineBuilder.builder();
 
+            /* (1)
+             * ~/.m2/repository/org/springframework/statemachine/spring-statemachine-core/2.0.2.RELEASE/spring-statemachine-core-2.0.2.RELEASE-sources.jar!/org/springframework/statemachine/config/StateMachineBuilder.java:153
+             * each new builder.build() calls "new ConcurrentTaskScheduler()"
+             */
             builder.configureConfiguration()
                     // uncomment to enable custom executor
-//                    .taskScheduler(stateMachineTaskScheduler())
-                    .withConfiguration().machineId("Test-" + i);
+                    .withConfiguration().machineId("Test-" + i)
+                    /* (2)
+                     * So we MUST configure manually, but we need singleton , so need to inject BEAN
+                     */
+                    .taskScheduler(scheduler)
+            ;
 
             builder.configureStates()
                     .withStates()
